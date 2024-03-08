e5fa04370885828218cd329b34a0685b472a1899
Ibrahimsuf <110345830+Ibrahimsuf@users.noreply.github.com>
2024-03-08 14:58:01 -0600

    Add sales summary and sales report functions

--- sales_report.c
+++ sales_report.c
@@ -1,10 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+void printMonthlySalesReport(float sales[12], char *months[12]) {
+  printf("Monthly sales report for 2022:\n");
+  printf("\n");
+  printf("Month     Sales\n");
+  for (int i = 0; i < 12; i++) {
+    fputs(months[i], stdout);
+    for (int j = strlen(months[i]) - 1; j < 9; j++) {
+      fputs(" ", stdout);
+    }
+    printf("$%.2f\n", sales[i]);
+  }
+}
+
+void printSalesSummary(float sales[12], char *months[12]) {
+  float average = sales[0] / 12;
+  float max = sales[0];
+  int max_month = 0;
+  float min = sales[0];
+  int min_month = 0;
+  for (int i = 0; i < 12; i++) {
+    average += sales[i] / 12;
+    if (sales[i] > max) {
+      max = sales[i];
+      max_month = i;
+    }
+    if (sales[i] < min) {
+      min = sales[i];
+      min_month = i;
+    }
+  }
+  
+  printf("Sales summary:\n");
+  printf("Minimum sales: $%.2f\t(%s)\n", min, months[min_month]);
+  printf("Maximum sales: $%.2f\t(%s)\n", max, months[max_month]);
+  printf("Average sales: $%.2f\n", average);
+}
 
 int main() {
   FILE *fptr;
 
-  fptr = fopen("sales_file.txt", "w");
+  fptr = fopen("sales_file.txt", "r");
   
   float sales[12];
   
@@ -14,11 +52,13 @@ int main() {
 
   fclose(fptr);
 
+  char *months[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
-  // print out each line in the file
-  for(int i = 0; i < 12; i++) {
-    printf("%f\n", sales[i]);
-  }
-
-  fclose(fptr);
+  printMonthlySalesReport(sales, months);
+  printf("\n");
+  printSalesSummary(sales, months);
+  // printSixMonthMovingAverage(sales);
+  // printSalesReportSorted(sales);
 }
+
+
